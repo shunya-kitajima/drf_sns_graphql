e97180c4a5b07c99ff1efdb8bd6fe00fee8aafb2
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-11 11:08:21 +0900

    add setting for AUTHENTICATION_BACKENDS

--- sns_graphql/settings.py
+++ sns_graphql/settings.py
@@ -66,6 +66,11 @@ GRAPHENE = {
     ],
 }
 
+AUTHENTICATION_BACKENDS = [
+    "graphql_jwt.backends.JSONWebTokenBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
+
 ROOT_URLCONF = "sns_graphql.urls"
 
 TEMPLATES = [
