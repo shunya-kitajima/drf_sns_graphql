f8e74399662a19566d28bebc5f90cbe8d9cfa214
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-11 11:13:32 +0900

    create Profile class

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user_prof = models.OneToOneField(
+        User,
+        related_name="profile",
+        on_delete=models.CASCADE()
+    )
+    followings = models.ManyToManyField(
+        User,
+        related_name="profiles_followings",
+        blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
