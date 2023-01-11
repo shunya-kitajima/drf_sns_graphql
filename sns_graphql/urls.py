b27adb3fd18a063ea7647de8fb8994b572cf513c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-11 14:35:12 +0900

    import modules

--- sns_graphql/urls.py
+++ sns_graphql/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+from sns_graphql.schema import schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
