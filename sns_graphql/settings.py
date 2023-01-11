29e78d6732dd1b158d92403c44ead7db8b9e7bc2
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-11 12:57:28 +0900

    modify module name

--- sns_graphql/settings.py
+++ sns_graphql/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "graphene.django",
+    "graphene_django",
     "api.apps.ApiConfig",
     "corsheaders",
 ]
